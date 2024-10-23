f94e9f5a18a7b5f4911c5bfb5830ebf4bb0a770e
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2024-10-23 16:45:40 -0400

    Update problem lists

--- hotspotjtregj9.sh
+++ hotspotjtregj9.sh
@@ -5,4 +5,4 @@ EXTENSIONSBASEDIR=$(pwd)
 TEST_JDK_HOME=$EXTENSIONSBASEDIR/build/macosx-aarch64-server-release/images/jdk
 
 # run-test-prebuilt runs tests without recompiling the jdk
-make run-test-prebuilt TEST="hotspot_valhalla" BOOT_JDK=$TEST_JDK_HOME JT_HOME=$JTREGHOME JDK_IMAGE_DIR=$TEST_JDK_HOME TEST_IMAGE_DIR="$EXTENSIONSBASEDIR/build/macosx-aarch64-server-release/images/test" JTREG_EXTRA_PROBLEM_LISTS="$EXTENSIONSBASEDIR/ProblemList-J9Valhalla.txt" JTREG_JAVA_OPTIONS="-XX:ValueTypeFlatteningThreshold=99999 -XX:+EnableArrayFlattening"
+make run-test-prebuilt TEST="hotspot_valhalla" BOOT_JDK=$TEST_JDK_HOME JT_HOME=$JTREGHOME JDK_IMAGE_DIR=$TEST_JDK_HOME TEST_IMAGE_DIR="$EXTENSIONSBASEDIR/build/macosx-aarch64-server-release/images/test" JTREG_EXTRA_PROBLEM_LISTS="$EXTENSIONSBASEDIR/ProblemList-hotspotjtreg.txt" JTREG_JAVA_OPTIONS="-XX:ValueTypeFlatteningThreshold=99999 -XX:+EnableArrayFlattening"
