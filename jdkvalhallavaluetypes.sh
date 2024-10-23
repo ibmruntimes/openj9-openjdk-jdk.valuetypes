c4109cc47ef5b3bbd224b12bfb43ad89cf9785d2
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2024-10-23 16:15:23 -0400

    Scripts to run all valhalla tests

---
+++ jdkvalhallavaluetypes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+JTREGHOME="/Users/theresamammarella/jtreg"
+EXTENSIONSBASEDIR=$(pwd)
+TEST_JDK_HOME=$EXTENSIONSBASEDIR/build/macosx-aarch64-server-release/images/jdk
+
+# run-test-prebuilt runs tests without recompiling the jdk
+make run-test-prebuilt TEST="jdk_valhalla" BOOT_JDK=$TEST_JDK_HOME JT_HOME=$JTREGHOME JDK_IMAGE_DIR=$TEST_JDK_HOME TEST_IMAGE_DIR="$EXTENSIONSBASEDIR/build/macosx-aarch64-server-release/images/test" JTREG_EXTRA_PROBLEM_LISTS="$EXTENSIONSBASEDIR/ProblemList-J9Valhalla.txt" JTREG_JAVA_OPTIONS="-XX:ValueTypeFlatteningThreshold=99999 -XX:+EnableArrayFlattening"
