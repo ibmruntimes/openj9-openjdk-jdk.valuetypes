51685f0ad55883c27867fee2d414762cd3edf798
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2024-10-24 11:02:59 -0400

    Cleanup and add comment in scripts for migrated value classes

--- hotspotjtregj9.sh
+++ hotspotjtregj9.sh
@@ -3,6 +3,17 @@
 JTREGHOME="/Users/theresamammarella/jtreg"
 EXTENSIONSBASEDIR=$(pwd)
 TEST_JDK_HOME=$EXTENSIONSBASEDIR/build/macosx-aarch64-server-release/images/jdk
+TEST_IMAGE=$EXTENSIONSBASEDIR/build/macosx-aarch64-server-release/images/test
+
+# https://github.com/eclipse-openj9/openj9/issues/20386
+# Add --patch-module java.base=$EXTENSIONSBASEDIR/build/macosx-aarch64-server-release/jdk/lib/valueclasses/java.base-valueclasses.jar
 
 # run-test-prebuilt runs tests without recompiling the jdk
-make run-test-prebuilt TEST="hotspot_valhalla" BOOT_JDK=$TEST_JDK_HOME JT_HOME=$JTREGHOME JDK_IMAGE_DIR=$TEST_JDK_HOME TEST_IMAGE_DIR="$EXTENSIONSBASEDIR/build/macosx-aarch64-server-release/images/test" JTREG_EXTRA_PROBLEM_LISTS="$EXTENSIONSBASEDIR/ProblemList-hotspotjtreg.txt" JTREG_JAVA_OPTIONS="-XX:ValueTypeFlatteningThreshold=99999 -XX:+EnableArrayFlattening"
+make run-test-prebuilt \
+    TEST="hotspot_valhalla" \
+    JTREG_EXTRA_PROBLEM_LISTS="$EXTENSIONSBASEDIR/ProblemList-hotspotjtreg.txt" \
+    JTREG_JAVA_OPTIONS="-XX:ValueTypeFlatteningThreshold=99999 -XX:+EnableArrayFlattening" \
+    BOOT_JDK=$TEST_JDK_HOME \
+    JT_HOME=$JTREGHOME \
+    JDK_IMAGE_DIR=$TEST_JDK_HOME \
+    TEST_IMAGE_DIR=$TEST_IMAGE
